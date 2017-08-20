ec9251750500bc1c4cb30f478f9570eb7d7479d6
hztankaiwen <hztankaiwen@corp.netease.com>
2017-08-20 20:18:23 +0800

    fixbug:toc position error

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -11,104 +11,101 @@ $(function() {
   $('.modal').modal();
 });
 
-var setTocToggle = (function() {
-  var onscrollSelect = true,
-    correction = 0;
+var setTocToggle = function() {
+  var onscrollSelect = true;
+  var $toc = $('.toc-panel nav');
+  var $footer = $('footer.page-footer');
+  var $post = $('.post .card');
+  var $header = $('nav.navbar');
+  var items = [].slice.call($toc.find('li a'));
+
+  var tocHeight = $toc.outerHeight(); // 包括内边距
+  var postOffset = $post.offset();
+  var headerHeight = $header.height();
+  var correction = headerHeight + 20;
+  var anchor = getAnchor();
+  //function animate above will convert float to int.
+  function getAnchor() {
+    return items.map(function(elem) {
+      return Math.floor($(elem.getAttribute('href')).offset().top - correction);
+    });
+  }
+
   function scrolltoElement(elem, cb) {
     var $elem = elem.href ? $(elem.getAttribute('href')) : $(elem);
     $('html, body').animate(
-      { scrollTop: $elem.offset().top - correction },
+      { scrollTop: $elem.offset().top - correction + 1 },
       400,
       cb
     );
   }
 
-  function _setTocToggle() {
-    var $toc = $('.toc-panel nav');
-    var $footer = $('footer.page-footer');
-    var $post = $('.post .card');
-    var $header = $('nav.navbar');
-    if ($toc.length === 0) return;
+  if ($toc.length === 0) return;
 
-    $toc.on('click', 'a', function(e) {
-      e.preventDefault();
-      e.stopPropagation();
-      onscrollSelect = false;
-      var $elem = $(this);
-      $toc.find('a').removeClass('active');
-      scrolltoElement(this, function() {
-        onscrollSelect = true;
-        $elem.addClass('active');
-      });
+  $toc.on('click', 'a', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    onscrollSelect = false;
+    var $elem = $(this);
+    $toc.find('a').removeClass('active');
+    scrolltoElement(this, function() {
+      onscrollSelect = true;
+      $elem.addClass('active');
     });
+  });
 
-    var items = [].slice.call($toc.find('li a'));
-    //function animate above will convert float to int.
-    var getAnchor = function() {
-      return items.map(function(elem) {
-        return Math.floor(
-          $(elem.getAttribute('href')).offset().top - correction
-        );
-      });
-    };
-    var anchor = getAnchor();
-    var tocOffset = $toc.offset();
-    var tocHeight = $toc.outerHeight(); // 包括内边距
-    var headerHeight = $header.height();
-    var scrollListener = function() {
-      if (!anchor) return;
-      var postHeight = $post.height();
-      var postOffset = $post.offset();
-      var scrollTop = $('html').scrollTop() || $('body').scrollTop();
-      var isset = false;
-      if (scrollTop + headerHeight >= tocOffset.top) {
-        $toc.removeClass('absolute').addClass('fixed').css('top', headerHeight);
-        isset = true;
-      }
-      if (scrollTop + headerHeight + tocHeight >= postOffset.top + postHeight) {
-        $toc
-          .removeClass('fixed')
-          .addClass('absolute')
-          .css({ top: postHeight - tocHeight });
-        isset = true;
-      }
-      if (!isset) {
-        tocOffset = $toc.offset();
-        $toc
-          .removeClass('fixed')
-          .removeClass('absolute')
-          .css({ top: 'initial' });
-      }
+  var scrollListener = function() {
+    if (!anchor) return;
+    var postHeight = $post.height();
+    var scrollTop = $('html').scrollTop() || $('body').scrollTop();
+    var isset = false;
 
-      if (onscrollSelect) {
-        //binary search.
-        var l = 0,
-          r = anchor.length - 1,
-          mid;
-        while (l < r) {
-          mid = (l + r + 1) >> 1;
-          if (anchor[mid] === scrollTop) l = r = mid;
-          else if (anchor[mid] < scrollTop) l = mid;
-          else r = mid - 1;
-        }
-        $(items).removeClass('active').eq(l).addClass('active');
-      }
-    };
+    if (scrollTop + headerHeight >= postOffset.top) {
+      $toc.removeClass('absolute').addClass('fixed').css('top', headerHeight);
+      isset = true;
+    }
 
-    $(window)
-      .resize(function() {
-        // tocOffset = $toc.offset();
-        tocHeight = $toc.outerHeight(); // 包括内边距
-        headerHeight = $header.height();
-        scrollListener();
-      })
-      .scroll(function() {
-        scrollListener();
-      });
+    if (scrollTop + headerHeight + tocHeight >= postOffset.top + postHeight) {
+      $toc
+        .removeClass('fixed')
+        .addClass('absolute')
+        .css({ top: postHeight - tocHeight });
+      isset = true;
+    }
+    if (!isset) {
+      $toc.removeClass('fixed').removeClass('absolute').css({ top: 'initial' });
+    }
+    if (onscrollSelect) {
+      //binary search.
+      var l = 0,
+        r = anchor.length - 1,
+        mid;
+      while (l < r) {
+        mid = (l + r + 1) >> 1;
+        if (anchor[mid] === scrollTop) l = r = mid;
+        else if (anchor[mid] < scrollTop) l = mid;
+        else r = mid - 1;
+      }
+      $(items).removeClass('active').eq(l).addClass('active');
+    }
+  };
+
+  $(window).resize(function() {
+    anchor = getAnchor();
+    postOffset = $post.offset();
+    tocHeight = $toc.outerHeight(); // 包括内边距
+    headerHeight = $header.height();
+    correction = headerHeight + 20;
+    onscrollSelect = true;
     scrollListener();
-  }
-  return _setTocToggle;
-})();
+  });
+
+  $(window).scroll(function() {
+    scrollListener();
+  });
+
+  scrollListener();
+};
 
 var setAsideToggle = (function() {
   var $aside = $('aside.side-panel');
